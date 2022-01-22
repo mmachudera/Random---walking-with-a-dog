370c15073264a82006e16d224db2bbe16186549b
machuderam <96541649+mmachudera@users.noreply.github.com>
2022-01-22 14:36:30 +0000

    If condition

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ random_choice = random.randint(0, num_items -1)
 person_who_walk = names[random_choice]
 print(person_who_walk + " is going to walk with a dog today.")
 
+plastic_bag = input(person_who_walk + ", do you have a plactic bag?  Y or N ? ")
+if plastic_bag == "Y":
+  print("Great! In case of rain, take also an umbrella.")
+else:
+  print("Do you want to pay a ticket? Take a bag, please.")
+
+water = input("Did you pack some water? Y or N ? ")
+if water == "Y":
+  print("You are a good guardian! Have fun.")
+else:
+  print("Take water and harry up. Your doggy doesn't like to wait too long!")
+
